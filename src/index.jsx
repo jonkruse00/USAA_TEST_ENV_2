f7aa98953b248b9648b26b45e274d249ebb781f3
Jonathan Kruse <jonathankruse00@gmail.com>
2021-03-03 15:41:04 -0600

    update other files

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Page from './components/Page';
+import React from "react";
+import ReactDOM from "react-dom";
+import Page from "./components/Page";
 
-import './index.less';
+import "./index.less";
 
-ReactDOM.render(<Page />, document.getElementById('root'));
+ReactDOM.render(<Page />, document.getElementById("root"));
